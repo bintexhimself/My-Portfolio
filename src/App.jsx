a7c74dd5f0f4516cb010df07f5c0c5e27ea636bd
bintexhimself <binutirihabib@gmail.com>
2025-09-27 13:18:59 +0100

    AboutMe modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import AboutMe from "./components/Aboutme";
+import AboutMe from "./components/AboutMe";
 import Project from "./components/Project";
 import Footer from "./components/Footer";
 
